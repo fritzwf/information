c6326f5acb554983d7c4053f56b6d2bfc6c7af84
Fritz Feuerbacher <fritz.feuerbacher@gmail.com>
2022-12-30 16:41:44 -0500

    Added history component.

---
+++ src/app/history/history.component.ts
@@ -0,0 +1,52 @@
+import { DeviceDetectorService } from 'ngx-device-detector';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+import { Component, OnInit } from '@angular/core';
+
+import { NinjaApiService } from '../ninja-api.service';
+
+@Component({
+  selector: 'app-history',
+  templateUrl: './history.component.html',
+  styleUrls: ['./history.component.scss']
+})
+export class HistoryComponent implements OnInit {
+
+  isMobile = false;
+  histNotFound = false;
+  hist: any = [];
+  notFound = "n/a";
+
+  constructor(
+    private deviceService: DeviceDetectorService,
+    private spinner: NgxSpinnerService,
+    private ninjaService: NinjaApiService
+  ) { }
+
+  ngOnInit() {
+    if (this.deviceService.isMobile()) {
+      this.isMobile = true;
+    }
+  }
+
+  getHistory(activity: string) {
+    this.spinner.show();
+    this.ninjaService.getHistory(activity).then((result: any) => {
+      // console.warn("History: " + JSON.stringify(result));
+      this.histNotFound = false;
+      if (result) {
+        this.hist = result;
+        this.histNotFound = result.length ? false : true;
+      }
+      this.spinner.hide();
+    }).catch((err: any) => {
+      console.error("History retrieval failed.");
+    });
+
+    // Timeout if the BE gets held up.
+    setTimeout(() => {
+      this.spinner.hide();
+    }, 15000);
+  }
+
+}
